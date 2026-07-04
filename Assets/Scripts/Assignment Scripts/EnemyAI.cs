1315fb1a0acc74a1b47712935831e0cec177c501
unknown <mps.mularczyk@gmail.com>
2026-07-04 23:24:55 +0200

    Implement Spawn Manager

--- Assets/Scripts/Assignment Scripts/EnemyAI.cs
+++ Assets/Scripts/Assignment Scripts/EnemyAI.cs
@@ -8,9 +8,22 @@ public class EnemyAI : MonoBehaviour
     private NavMeshAgent _agent;
 
     [SerializeField] private int _currentWaypoint = 0;
-    [SerializeField] private List<Transform> _waypoints;
+    [SerializeField] private List<Transform> _waypoints = new List<Transform>();
 
 
+    private void Awake()
+    {
+        var waypointsHolder = GameObject.Find("Enemy AI waypoints");
+        for (int i = 0; i < waypointsHolder.transform.childCount; i++)
+        {
+            _waypoints.Add(waypointsHolder.transform.GetChild(i));
+        }
+        
+        if(_waypoints.Count < 2)
+        {
+            Debug.LogError("Not enough waypoints for enemy AI Agent");
+        }
+    }
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
@@ -23,6 +36,17 @@ public class EnemyAI : MonoBehaviour
         _agent.SetDestination(_waypoints[_currentWaypoint].position);
     }
 
+    private void OnEnable()
+    {
+        _currentWaypoint = 0;
+
+        if (_agent != null && _waypoints != null && _waypoints.Count > 0)
+        {
+            _agent.ResetPath();
+            _agent.SetDestination(_waypoints[0].position);
+        }
+    }
+
     void Update()
     {
         if (_agent.remainingDistance < 0.5f && _currentWaypoint < _waypoints.Count - 1)
@@ -30,5 +54,24 @@ public class EnemyAI : MonoBehaviour
             _currentWaypoint++;
             _agent.SetDestination(_waypoints[_currentWaypoint].position);
         }
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyEndPosition"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    /*
+    private void OnDisable()
+    {
+        _currentWaypoint = 0;
+
+        if (_agent != null)
+            _agent.ResetPath();
+    }
+    */
 }
---
+++ Assets/Scripts/Assignment Scripts/SpawnManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public static SpawnManager Instance { get; private set; }
+
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform _spawnLocation;
+
+    [SerializeField] private bool _isSpawning= true;
+    [SerializeField] private float _spawnTimeStep = 5f;
+
+    [SerializeField] private int _enemyPoolSize = 10;
+    private List<GameObject> _enemyPool = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        //DontDestroyOnLoad(gameObject);
+
+        for (int i = 0; i < _enemyPoolSize; i++)
+        {
+            GameObject enemy = Instantiate(_enemyPrefab, transform);
+            enemy.SetActive(false);
+            _enemyPool.Add(enemy);
+        }
+
+    }
+    void Start()
+    {
+        StartCoroutine(EnemySpawningRoutine());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SpawnEnemy()
+    {
+        //Instantiate(_enemyPrefab, _spawnLocation.position, Quaternion.identity);
+        //Debug.Log("Spawned enemy");
+        GetEnemyFromPool(_spawnLocation.position, Quaternion.identity);
+        Debug.Log("Spawned enemy from pool");
+    }
+
+    IEnumerator EnemySpawningRoutine()
+    {
+        while (_isSpawning)
+        {
+            yield return new WaitForSeconds(_spawnTimeStep);
+            SpawnEnemy();
+        }
+        
+    }
+
+    private GameObject GetEnemyFromPool(Vector3 position, Quaternion rotation)
+    {
+        // Get enemy object from pool if possible
+        foreach (GameObject enemy in _enemyPool)
+        {
+            if (!enemy.activeInHierarchy)
+            {
+                enemy.transform.position = position;
+                enemy.transform.rotation = rotation;
+                enemy.SetActive(true);
+                return enemy;
+            }
+        }
+
+        // If pool is too small - create new enemy object and add to pool
+        GameObject newEnemy = Instantiate(_enemyPrefab, position, rotation);
+        _enemyPool.Add(newEnemy);
+        return newEnemy;
+    }
+}
