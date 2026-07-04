6a31557180ce5835b08ddf1dcf36afb06d44465c
unknown <mps.mularczyk@gmail.com>
2026-07-05 00:32:06 +0200

    Implement Player shooting

---
+++ Assets/Scripts/Assignment Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 screenCenter = new Vector2(0.5f, 0.5f);
+
+            Ray rayOrigin = Camera.main.ViewportPointToRay(screenCenter);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity, 1 << 10))
+            {
+                Debug.Log("Hit: " + hitInfo.collider.gameObject.name);
+            }
+        }
+    }
+}
